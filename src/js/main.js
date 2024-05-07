b16b84031c366449fc2035dc9de6d56485930e43
Layane Costa <layane.andreia@hotmaill.com>
2024-05-06 22:56:13 -0300

    as quartas usamos rosa

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+const horaContainer = document.querySelectorAll(".time");
+const body = document.querySelector("body")
 const horas = document.getElementById("horas");
 const minutos = document.getElementById("minutos");
 const segundos = document.getElementById("segundos");
@@ -11,6 +13,16 @@ const relogio = setInterval(function time() {
     if(hora < 10) hora = "0" + hora;
     if(minuto < 10) minuto = "0" + minuto;
     if(segundo < 10) segundo = "0" + segundo;
+
+    if(dateToday.getDay() === 3)
+    {
+        for (let i = 0; i < horaContainer.length; i++) 
+        {
+            horaContainer[i].style.background = "#d93098";
+        }
+
+        body.style.background = "linear-gradient(120deg,#eb37c4,#f6a3e4, #eb37c4)";
+    }
 
     horas.textContent = hora;
     minutos.textContent = minuto;
